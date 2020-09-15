3afacb63764532c2198ea523e35b56ca6658ca0d
coolgsvirk <71279954+coolgsvirk@users.noreply.github.com>
2020-09-15 14:46:38 +0530

    Update Instructions.py

--- Instructions.py
+++ Instructions.py
@@ -1,3 +1,18 @@
-#Hi, you are probabaly new so I will help you for common errors
+#Hi, you are probabaly new so I will help you with the first step printing
+#For IDE I will recommend Pycharm Community edition, but you can use any IDE. First we will install Python 
+#For a better tutorial I will provide you a link 
+#We will start with hello world in python
+# For windows:https://www.youtube.com/watch?v=QMbnSoB1CRU
+#For MacOS : https://www.youtube.com/watch?v=2YxSCRrxu5U
+#For Linux Pyhton comes preinstalled mostly.
+#For installation on Pycharm 
+#For windows: https://www.youtube.com/watch?v=SZUNUB6nz3g
+#For macOS : https://www.youtube.com/watch?v=oyzH4M6X6F4
+# For linux: (NOTE: FOR UBUNTU AND LINUX MINT USERS YOU CAN USE THE PRE-INSTALLED APP STORE)Go to the Jetbarins official site and follow the instructions.
+#Done?, let's start programming!!!!!!!
 #We will start with hello world in python
 print("hello world")
+#here print is a COMMAND that enables us to just print anythibg but not on an paper but on a sheet, after that we put parethesis also known as brackets then ""
+# In betwwen the two "" we have to type what we nned to have in our output tab and then close the brackets # I use is for comments on the code that you are typing
+#Got IT? Congrats you have done your first python program.
+#This is a work in progress and hence, will be updated everyday.
